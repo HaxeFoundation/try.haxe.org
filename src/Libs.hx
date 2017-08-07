44f022a9916fc285135db07e46aba0ecc56065ba
MrCdK <contact@mrcdk.com>
2017-08-07 20:09:41 +0200

    Modified the libraries list

--- src/Libs.hx
+++ src/Libs.hx
@@ -17,68 +17,43 @@ typedef LibConf = {
 class Libs
 {
 
-	static var thx:Array<LibConf> = [
-		{name:"thx.core"},
-		{name:"thx.color"},
-		{name:"thx.promise"},
-		{name:"thx.stream"},
-		{name:"thx.culture"},
-		{name:"thx.stream.dom"},
-		{name:"thx.benchmark"},
-		{name:"thx.csv"},
-		{name:"thx.text"},
-	];
-
-	static var tink:Array<LibConf> = [
-		{name:"tink_core"},
-		{name:"tink_macro"},
-		{name:"tink_priority"},
-		{name:"tink_lang"},
-		{name:"tink_xml"},
-		{name:"tink_template"},
-		{name:"tink_concurrent"},
-		{name:"tink_streams"},
-		{name:"tink_io"},
-		{name:"tink_runloop"},
-		{name:"tink_tcp"},
-		{name:"tink_http"},
-		{name:"tink_url"},
-		{name:"tink_parse"},
-		{name:"tink_json"},
-		{name:"tink_clone"},
-		{name:"tink_await"},
-		{name:"tink_web"},
-		{name:"futurize"},
-	];
-
 	static var available : Map<String, Array<LibConf>> = [
 		"JS" => [
-			//{name:"nme", args : ["--remap","flash:browser"], head : ["<link rel='stylesheet' href='../swf.css' type='text/css'/>"], body:["<div id='haxe:jeash'></div>"]},
 			{name:"actuate" , help:"https://github.com/openfl/actuate",args : []},
-			//{name:"selecthx"},
-			//{name:"modernizr"},
-			//{name:"browserhx"},
 			{name:"format", help:"https://github.com/HaxeFoundation/format"},
 			{name:"tink_core", help:"https://github.com/haxetink/tink_core"},
 			{name:"tink_lang", help:"https://github.com/haxetink/tink_lang"},
+			{name:"tink_macro", help:"https://github.com/haxetink/tink_macro"},
 			{name:"thx.core", help:"https://github.com/fponticelli/thx.core"},
+			{name:"thx.culture", help:"https://github.com/fponticelli/thx.culture"},
 			{name:"hxColorToolkit", help:"https://github.com/andyli/hxColorToolkit"},
 			{name:"threejs", head: ["<script src='../../../lib/js/three.min.js'></script>"]},
 			{name:"pixijs", head: ["<script src='../../../lib/js/pixi.min.js'></script>"]},
 		],
-		"SWF" => new Array<LibConf>().concat([
+		"SWF" => [
 			{name:"actuate" , help:"https://github.com/openfl/actuate",args : []},
 			{name:"format", help:"https://github.com/HaxeFoundation/format"},
 			{name:"tink_core", help:"https://github.com/haxetink/tink_core"},
 			{name:"tink_lang", help:"https://github.com/haxetink/tink_lang"},
+			{name:"tink_macro", help:"https://github.com/haxetink/tink_macro"},
 			{name:"thx.core", help:"https://github.com/fponticelli/thx.core"},
+			{name:"thx.culture", help:"https://github.com/fponticelli/thx.culture"},
 			{name:"hxColorToolkit", help:"https://github.com/andyli/hxColorToolkit"},
 			{name:"away3d", swf:{src:"away3d4.swf"}, help:"http://away3d.com/livedocs/away3d/4.0/"},
-			//{name:"starling" },
-		])
+		],
+		"NEKO" => [
+			{name:"actuate" , help:"https://github.com/openfl/actuate",args : []},
+			{name:"format", help:"https://github.com/HaxeFoundation/format"},
+			{name:"tink_core", help:"https://github.com/haxetink/tink_core"},
+			{name:"tink_lang", help:"https://github.com/haxetink/tink_lang"},
+			{name:"tink_macro", help:"https://github.com/haxetink/tink_macro"},
+			{name:"thx.core", help:"https://github.com/fponticelli/thx.core"},
+			{name:"thx.culture", help:"https://github.com/fponticelli/thx.culture"},
+			{name:"hxColorToolkit", help:"https://github.com/andyli/hxColorToolkit"},
+		],
 	];
 
-	static var defaultChecked : Map < String, Array<String> > = ["JS" => [], "SWF" => []]; // array of lib names
+	static var defaultChecked : Map < String, Array<String> > = ["JS" => [], "SWF" => [], "NEKO" => []]; // array of lib names
 
 
 	static public function getLibsConfig(?target:Target, ?targetName:String):Array<LibConf>
