0090f9a6a9dabcea90daf52cd3045131f3c4d8da
AlexHaxe <Alexander.Blum@gmail.com>
2021-01-14 18:47:24 +0100

    added error handler to JSON sample

--- www/examples/Example-19.hx
+++ www/examples/Example-19.hx
@@ -1,12 +1,15 @@
 class Test {
-  static function main() {
-    var http = new haxe.Http("https://api.ipify.org?format=json");
-    http.onData = function(data) {
-      var result:IpAddress = haxe.Json.parse(data);
-      trace('Your IP-address: ${result.ip}');
-    }
-    http.request();
-  }
+	static function main() {
+		var http = new haxe.Http("https://api.ipify.org?format=json");
+		http.onData = function(data) {
+			var result:IpAddress = haxe.Json.parse(data);
+			trace('Your IP-address: ${result.ip}');
+		}
+		http.onError = function(e) {
+			trace(e);
+		}
+		http.request();
+	}
 }
 
-typedef IpAddress = { ip:String }
+typedef IpAddress = {ip:String}
